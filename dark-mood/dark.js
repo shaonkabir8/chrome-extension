afe3149472b63ddfb48591d0fa195c5eed389fae
Shaonkabir <shaonkabir98@gmail.com>
2020-03-03 16:34:12 +0600

    Feat: Dark Mood Switcher added. It's funny

---
+++ dark-mood/dark.js
@@ -0,0 +1,14 @@
+
+
+// Dark Mood Creation Concept:-
+// Thanks Coding_Train
+
+(function() {
+    let paragraphs = document.getElementsByTagName('p')
+    for(let i =0; i<paragraphs.length; i++) {
+        paragraphs[i].style = 'background: #ccc; color: #000; padding: 10px;'
+    }
+    document.body.style = 'background: #282c35; color: #ddd !important'
+})()
+
+
