91f746b0b553b1e6b6b392f2c0ad80129610a9e2
Shaonkabir <shaonkabir98@gmail.com>
2020-03-13 00:16:12 +0600

    refactor(newTab): add scripts for searching on google finally

--- new-tab/app.js
+++ new-tab/app.js
@@ -27,3 +27,24 @@ let year = dateObj.getFullYear();
 let output = `(${day} ${month}, ${year})`;
 
 document.getElementById("date").innerHTML = output;
+
+
+// Google Search
+
+// grab the elements to interact.
+const form = document.querySelector('.form');
+const input = document.querySelector('.form__input')
+
+// initial search query [it'll be update on typing]
+let searchQuery = '';
+
+// Input Handler
+input.addEventListener('change', e => {
+  searchQuery += e.target.value;
+})
+
+// Submit Handler
+form.addEventListener('submit', function(e) {
+  // redirect to google with query string
+  window.location.replace(`https://google.com/search?q=${searchQuery}`)
+})
