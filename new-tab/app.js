65e257e9e27d0b24b2d45d62a3955e0bd320068e
Shaonkabir <shaonkabir98@gmail.com>
2020-03-12 21:54:31 +0600

    refactor: separate files for clean code

---
+++ new-tab/app.js
@@ -0,0 +1,30 @@
+// Running Clock
+setInterval(() => {
+  const time = document.getElementById(
+    "time"
+  ).innerHTML = new Date().toLocaleTimeString();
+});
+
+
+// Current Day, Month and Year
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+let dateObj = new Date();
+let month = monthNames[dateObj.getMonth()];
+let day = String(dateObj.getDate()).padStart(2, "0");
+let year = dateObj.getFullYear();
+let output = `(${day} ${month}, ${year})`;
+
+document.getElementById("date").innerHTML = output;
